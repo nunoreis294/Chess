3cb2fa9c79c84b6b674d0054da29c0f739aacfeb
nunoreis294 <nunoreis294@gmail.com>
2026-07-01 08:49:14 +0100

    Add move history and algebraic notation to Board
    Introduce a Move struct and moveHistory vector to track moves with algebraic notation, including check and checkmate. Update move logic to record moves, add helpers for square notation, and stub out check, checkmate, and castling logic. Add Bugs.md to project files.

--- Chess/board.cpp
+++ Chess/board.cpp
@@ -33,16 +33,78 @@ void Board::reset()
 	}
 
 	board[2][2] = { PieceType::Pawn, PieceColor::White };
+
+	moveHistory.clear();
 }
 
 // Move a piece from (fromX, fromY) to (toX, toY)
 bool Board::move(int fromX, int fromY, int toX, int toY)
 {
+	int number = (int)moveHistory.size() + 1;
+	bool isCheck = false;
+	bool isCheckmate = false;
+	bool isCapture = board[toY][toX].type != PieceType::None;
+	std::string notation = "";
+
+	Piece movedPiece = board[fromY][fromX];
+
+	std::string fromSquare = getSquareNotation(fromX, fromY);
+	std::string toSquare = getSquareNotation(toX, toY);
+
 	PieceType pieceType = board[fromY][fromX].type;
 	PieceColor pieceColor = board[fromY][fromX].color;
 
 	board[toY][toX] = { pieceType, pieceColor };
 	board[fromY][fromX] = { PieceType::None, PieceColor::None };
+
+	if (pieceColor == PieceColor::White)
+		notation = std::to_string(number) + ". ";
+
+	if (pieceType != PieceType::Pawn)
+		notation += pieceType == PieceType::Knight ? 'N' :
+		pieceType == PieceType::Bishop ? 'B' :
+		pieceType == PieceType::Rook ? 'R' :
+		pieceType == PieceType::Queen ? 'Q' : 'K';
+
+	if (isCapture)
+	{
+		char captureChar = fromSquare[0];
+		std::vector<sf::Vector2i> piecesOfSameType = getAttackingPieces(pieceColor, pieceType, toX, toY);
+
+		if (piecesOfSameType.size() > 1)
+		{
+			for (sf::Vector2i pieceOfSameType : piecesOfSameType)
+			{
+				if (getSquareNotation(toX, toY)[0] == captureChar)
+					captureChar = fromSquare[1];
+			}
+
+			notation += captureChar;
+		}
+
+		notation += 'x';
+	}
+
+	notation += toSquare;
+
+	if (isKingCheckMated(pieceColor == PieceColor::White ? PieceColor::Black : PieceColor::White))
+	{
+		isCheckmate = true;
+		notation += '#';
+	}
+
+	if (!isCheckmate && (isKingChecked(pieceColor == PieceColor::White ? PieceColor::Black : PieceColor::White)))
+	{
+		isCheck = true;
+		notation += '+';
+	}
+
+	if (pieceColor == PieceColor::White)
+		moveHistory.push_back({ notation, number, board[toY][toX], fromSquare, isCapture, toSquare, isCheck, isCheckmate });
+	else
+	{
+		moveHistory.back().notation += " " + notation;
+	}
 
 	return true;
 }
@@ -53,10 +115,31 @@ Piece Board::getPiece(int x, int y) const
 	return board[y][x];
 }
 
+std::vector<Move> Board::getMoveHistory() const
+{
+	return moveHistory;
+}
+
 // Check if the king of the given color is in check
 bool Board::isKingChecked(PieceColor pieceColor) const
 {
+	//TODO: Implement check detection logic
 	return false;
+}
+
+// Check if the king of the given color is in checkmate
+bool Board::isKingCheckMated(PieceColor pieceColor) const
+{
+	//TODO: Implement checkmate detection logic
+	return false;
+}
+
+std::vector<sf::Vector2i> Board::getAttackingPieces(PieceColor pieceColor, PieceType pieceType, int toX, int toY) const
+{
+	//TODO: Implement logic to find all pieces of the given type and color that can attack the square (toX, toY) excluding the piece that is currently on that square
+
+
+	return std::vector<sf::Vector2i>();
 }
 
 // Get possible squares for the selected piece
@@ -209,6 +292,12 @@ std::vector<sf::Vector2i> Board::getPossibleSquares(sf::Vector2i selectedPiece) 
 		}
 			break;
 		case PieceType::King: {
+			if (isKingChecked(piece.color))
+			{
+				// King cannot move to a square that is under attack
+				break;
+			}
+
 			int directions[8][2] = {
 				{ 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 },
 				{ 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 }
@@ -229,7 +318,43 @@ std::vector<sf::Vector2i> Board::getPossibleSquares(sf::Vector2i selectedPiece) 
 				}
 			}
 
-			//Castling
+			//TODO: Implement castling logic
+			//Castling consists of moving a king two squares towards the rook and executing a jump of a rook over the king.
+			//the king and the relevant rook must not be moved, considered as castling rights inside a chess position
+			//the king must not be in check nor may the king pass through a square that is attacked by an enemy piece
+			//no square between king's start and final square may be controlled by the enemy
+			if (piece.color == PieceColor::White && selectedPiece == sf::Vector2i(5, 1))
+			{
+				// Check for white castling
+				if (board[0][4].type == PieceType::Rook && board[0][4].color == PieceColor::White &&
+					board[0][3].type == PieceType::None && board[0][2].type == PieceType::None &&
+					!isKingChecked(PieceColor::White))
+				{
+					possibleSquares.push_back(sf::Vector2i(3, 1)); // Queenside castling
+				}
+				if (board[0][7].type == PieceType::Rook && board[0][7].color == PieceColor::White &&
+					board[0][6].type == PieceType::None &&
+					!isKingChecked(PieceColor::White))
+				{
+					possibleSquares.push_back(sf::Vector2i(7, 1)); // Kingside castling
+				}
+			}
+			else if (piece.color == PieceColor::Black && selectedPiece == sf::Vector2i(5, 8))
+			{
+				// Check for black castling
+				if (board[7][4].type == PieceType::Rook && board[7][4].color == PieceColor::Black &&
+					board[7][3].type == PieceType::None && board[7][2].type == PieceType::None &&
+					!isKingChecked(PieceColor::Black))
+				{
+					possibleSquares.push_back(sf::Vector2i(3, 8)); // Queenside castling
+				}
+				if (board[7][7].type == PieceType::Rook && board[7][7].color == PieceColor::Black &&
+					board[7][6].type == PieceType::None &&
+					!isKingChecked(PieceColor::Black))
+				{
+					possibleSquares.push_back(sf::Vector2i(7, 8)); // Kingside castling
+				}
+			}
 		}
 			break;
 		default:
@@ -278,7 +403,7 @@ std::vector<sf::Vector2i> Board::getAttackedSquares(sf::Vector2i selectedPiece) 
 				}
 			}
 
-			//en passant
+			//TODO: Implement en passant attack detection logic
 		}
 			break;
 		case PieceType::Knight: {
@@ -403,7 +528,12 @@ std::vector<sf::Vector2i> Board::getAttackedSquares(sf::Vector2i selectedPiece) 
 		}
 			break;
 		case PieceType::King: {
-			//ckeck for check
+			if (isKingChecked(piece.color))
+			{
+				// King cannot move to a square that is under attack
+				break;
+			}
+
 			int directions[8][2] = {
 				{ 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 },
 				{ 1, 1 }, { 1, -1 }, { -1, 1 }, { -1, -1 }
@@ -437,4 +567,12 @@ void Board::setPiece(int x, int y, Piece piece) {
     board[y][x] = piece;
 
 	return;
+}
+
+std::string Board::getSquareNotation(int x, int y) const
+{
+	std::string notation;
+	notation += (char)('a' + (x));
+	notation += (char)('8' - (y));
+	return notation;
 }
--- Chess/board.h
+++ Chess/board.h
@@ -2,17 +2,34 @@
 #include <SFML/Graphics.hpp>
 #include "piece.h"
 
+
+struct Move {
+    std::string notation;
+    int number;
+    Piece movedPiece;
+    std::string fromSquare;
+	bool isCapture;
+    std::string toSquare;
+	bool isCheck;
+	bool isCheckmate;
+};
+
 class Board {
 public:
     Board();
     void reset();
     bool move(int fromX, int fromY, int toX, int toY);
     Piece getPiece(int x, int y) const;
+    std::vector<Move> getMoveHistory() const;
     bool isKingChecked(PieceColor pieceColor) const;
+    bool isKingCheckMated(PieceColor pieceColor) const;
+    std::vector<sf::Vector2i> getAttackingPieces(PieceColor pieceColor, PieceType pieceType, int toX, int toY) const;
     std::vector<sf::Vector2i> getPossibleSquares(sf::Vector2i selectedSquare) const;
     std::vector<sf::Vector2i> getAttackedSquares(sf::Vector2i selectedSquare) const;
     void setPiece(int x, int y, Piece piece);
+    std::string getSquareNotation(int x, int y) const;
 
 private:
     Piece board[8][8];
+    std::vector<Move> moveHistory;
 };
